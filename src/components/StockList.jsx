a5c25b5212954584ec1859a49f2a5a2f271e20ad
Tafita Hery <tafitahery@gmail.com>
2023-01-14 16:49:17 +0300

    Rendering table of stock

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -32,5 +32,36 @@ export default function StockList() {
     return () => (isMounted = false);
   }, [watchList]);
 
-  return <div>StockList</div>;
+  return (
+    <div>
+      <table className="table hover mt-5">
+        <thead style={{ color: 'rgb(79,89,102)' }}>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Last</th>
+            <th scope="col">Chg</th>
+            <th scope="col">Chg%</th>
+            <th scope="col">High</th>
+            <th scope="col">Low</th>
+            <th scope="col">Open</th>
+            <th scope="col">Pclose</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stock.map((stockData) => (
+            <tr className="table-row" key={stockData.symbol}>
+              <th scope="row">{stockData.symbol}</th>
+              <td>{stockData.data.c}</td>
+              <td>{stockData.data.d}</td>
+              <td>{stockData.data.dp}</td>
+              <td>{stockData.data.h}</td>
+              <td>{stockData.data.l}</td>
+              <td>{stockData.data.o}</td>
+              <td>{stockData.data.pc}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
